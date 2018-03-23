73fe8a48c49ae45b5557addb7e2f575b40314363
nhoening <iam@nicolashoening.de>
2018-03-23 10:15:36 +0100

    enable c+<n> and d+<n> shortcuts

--- static/js/scc.js
+++ static/js/scc.js
@@ -1,75 +1,100 @@
 
 // Shortcuts
 
-$(document).keydown(function(e) {
+var keysDown = {};
+
+var enterKeyCode = 13;
+var cKeyCode = 67;
+var dKeyCode = 68;
+var oneKeyCode = 49;
+
+$(document).keydown(function (e) {
+    keysDown[e.which] = true;
+    executeShortCuts();
+});
+
+$(document).keyup(function (e) {
+    delete keysDown[e.which];
+    executeShortCuts();
+});
+
+function executeShortCuts() {
 
     // Move to next on Enter
-    if (e.which == 13) {
+    if (enterKeyCode in keysDown) {
         console.log("Enter key pressed - clicking next move.");
         $("#next_turn").click();
     }
 
     // Station 1
-    if (e.which == 48 + 1) {
-        console.log("Shortcut for charging car 1.");
-        var btn = $('#add_one_ChargingStation0');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
+    if (oneKeyCode in keysDown){
+        if (cKeyCode in keysDown) {
+            console.log("Shortcut for charging car at station 1.");
+            var btn = $('#add_one_ChargingStation0');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
         }
-    }
-    if (e.which == 48 + 1 + 4) {
-        console.log("Shortut for discharging car 1.");
-        var btn = $('#remove_one_ChargingStation0');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
+        if (dKeyCode in keysDown) {
+            console.log("Shortcut for discharging car at station 1.");
+            var btn = $('#remove_one_ChargingStation0');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
         }
     }
 
     // Station 2
-    if (e.which == 48 + 2) {
-        console.log("Shortcut for charging car 2.");
-        var btn = $('#add_one_ChargingStation1');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
+    if (oneKeyCode + 1 in keysDown){
+        if (cKeyCode in keysDown) {
+            console.log("Shortcut for charging car at station 2.");
+            var btn = $('#add_one_ChargingStation1');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
         }
-    }
-    if (e.which == 48 + 2 + 4) {
-        console.log("Shortut for discharging car 2.");
-        var btn = $('#remove_one_ChargingStation1');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
+        if (dKeyCode in keysDown) {
+            console.log("Shortcut for discharging car at station 2.");
+            var btn = $('#remove_one_ChargingStation1');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
         }
     }
 
     // Station 3
-    if (e.which == 48 + 3) {
-        console.log("Shortcut for charging car 3.");
-        var btn = $('#add_one_ChargingStation2');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
+    if (oneKeyCode + 2 in keysDown){
+        if (cKeyCode in keysDown) {
+            console.log("Shortcut for charging car at station 3.");
+            var btn = $('#add_one_ChargingStation2');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
         }
-    }
-    if (e.which == 48 + 3 + 4) {
-        console.log("Shortut for discharging car 3.");
-        var btn = $('#remove_one_ChargingStation2');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
+        if (dKeyCode in keysDown) {
+            console.log("Shortcut for discharging car at station 3.");
+            var btn = $('#remove_one_ChargingStation2');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
         }
-    }
+       }
 
     // Station 4
-    if (e.which == 48 + 4) {
-        console.log("Shortcut for charging car 4.");
-        var btn = $('#add_one_ChargingStation3');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
+    if (oneKeyCode + 3 in keysDown){
+        if (cKeyCode in keysDown) {
+            console.log("Shortcut for charging car at station 4.");
+            var btn = $('#add_one_ChargingStation3');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
+        }
+        if (dKeyCode in keysDown) {
+            console.log("Shortcut for discharging car at station 4.");
+            var btn = $('#remove_one_ChargingStation3');
+            if (btn && !btn.prop('disabled')) {
+                btn.click();
+            }
         }
     }
-    if (e.which == 48 + 4 + 4) {
-        console.log("Shortut for discharging car 4.");
-        var btn = $('#remove_one_ChargingStation3');
-        if (btn && !btn.prop('disabled')) {
-            btn.click();
-        }
-    }
-});
+}
